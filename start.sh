ae8641a4113a063332a82b369409fcece7e2b2f4
brodan <christopher.hranj@gmail.com>
2015-07-10 12:17:56 -0700

    updated shell script

--- start.sh
+++ start.sh
@@ -2,3 +2,9 @@
 
 clear
 echo "Downloading docker-compose.yml"
+
+wget https://raw.githubusercontent.com/TwilioDevEd/account-verification-node/dockerize/docker-compose.yml
+
+docker compose up -f docker-compose-demo.yml -d
+
+echo "Your app is now running. Check the Github README to see which port to access."
